00740817b052cd6975355ce110a40b4b6e92b4a9
Liam McCartney <liam.mccartney@gmail.com>
2014-11-21 17:43:26 -0500

    write photos routes for uploading

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,1 +1,21 @@
-get 
+get '/photos/upload' do
+   @album = Album.find(params[:album][:id])
+
+  erb :'photos/uploader'
+end
+
+post '/photos/upload' do
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+  params[:photo][:photo_url] = "/img/#{@filename}"
+
+  File.open("public/img/#{@filename}", 'wb') do |f|
+    f.write(file.read)
+  end
+
+  album = Album.find(params[:id])
+  album.photos.create(params[:photo])
+
+  redirect("/albums/#{album.id}")
+
+end
