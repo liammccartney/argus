c99e3140f34a952692b72b3260a50213be91704d
Liam McCartney <liam.mccartney@gmail.com>
2014-11-21 14:34:15 -0500

    fix weird error

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
-
+class Album < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   has_many :photos
   validates :album_name, presence: true
-class Album < ActiveRecord::Base
+
 end
