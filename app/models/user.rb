55534491d8d74ce89e4ba81785e6b849c88bfd5e
Liam McCartney <liam.mccartney@gmail.com>
2014-11-22 20:51:25 -0500

    delete comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create}
-  ##validate email format
 end
