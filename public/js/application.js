3ccd7684db31e93782f218a98dfe9e2aa18d5b36
Liam McCartney <liam.mccartney@gmail.com>
2014-11-23 22:10:39 -0500

    removed uneccessary jquery

--- public/js/application.js
+++ public/js/application.js
@@ -4,7 +4,4 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
-  $('input[type=file]').bootstrapFileInput();
-$('.file-inputs').bootstrapFileInput();
 });
